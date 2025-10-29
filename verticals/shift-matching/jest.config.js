7a079a904899438016bba502b4dccc39d1c07501
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-10-29 14:02:26 -0500

    passWithNoTests for now

--- verticals/shift-matching/jest.config.js
+++ verticals/shift-matching/jest.config.js
@@ -13,4 +13,5 @@ module.exports = {
     '@care-commons/caregiver-staff': '<rootDir>/../caregiver-staff/src',
     '@care-commons/scheduling-visits': '<rootDir>/../scheduling-visits/src',
   },
+  passWithNoTests: true,
 };
