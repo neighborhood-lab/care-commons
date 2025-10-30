0cb0356e26e1b304cc998d1d717d4e36c36d5e34
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-10-30 16:56:06 -0500

    remove compiled migration files from git

--- packages/core/migrations/20251030214719_state_specific_fields.d.ts
+++
@@ -1,4 +0,0 @@
-import { type Knex } from 'knex';
-export declare function up(knex: Knex): Promise<void>;
-export declare function down(knex: Knex): Promise<void>;
-//# sourceMappingURL=20251030214719_state_specific_fields.d.ts.map
--- packages/core/migrations/20251030214721_state_specific_care_plans.d.ts
+++
@@ -1,4 +0,0 @@
-import { type Knex } from 'knex';
-export declare function up(knex: Knex): Promise<void>;
-export declare function down(knex: Knex): Promise<void>;
-//# sourceMappingURL=20251030214721_state_specific_care_plans.d.ts.map
