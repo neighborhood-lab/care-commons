7c898c2713fe2626e63d20f65369dc48ff60b831
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-10-30 18:19:08 -0500

    fix lint in care-commons/app

--- packages/app/src/middleware/request-logger.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response } from 'express';
-export declare function createRequestLogger(): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>, callback: (err?: Error) => void) => void;
-//# sourceMappingURL=request-logger.d.ts.map
