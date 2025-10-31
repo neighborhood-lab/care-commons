7263b7edb68d74341cd1a6efe6ae0c3c2654228f
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-10-31 09:18:48 -0500

    check script

---
+++ check.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eux
+
+find . -type f -name "package-lock.json" -exec rm -f {} +
+find . -type d -name "node_modules" -exec rm -rf {} +
+ncu -u --packageFile '**/package.json'
+npm install
+npm run db:nuke
+npm run db:migrate
+npm run db:seed
+npm run build
+npm run lint
+npm run typecheck
+npm test
