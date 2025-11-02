da4edd38ababbcdd314c2976d969fc4e2c7b10c5
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-11-02 09:40:08 -0600

    vitest-dom-matchers

--- packages/web/src/test/setup.ts
+++ packages/web/src/test/setup.ts
@@ -1,6 +1,6 @@
 import { afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
+import '@vitest/browser/matchers';
 
 // Cleanup after each test case
 afterEach(() => {
