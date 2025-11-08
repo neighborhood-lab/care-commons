87c4a66acae06b1227b661e7bf20a81c464934eb
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-11-07 20:15:56 -0600

    Remove unused FC import from EVVRecordCard
    - Remove unused FC type import to fix TypeScript warning
    - This resolves linting issue in time-tracking-evv components

--- packages/web/src/verticals/time-tracking-evv/components/EVVRecordCard.tsx
+++ packages/web/src/verticals/time-tracking-evv/components/EVVRecordCard.tsx
@@ -1,4 +1,4 @@
-import React, { type FC } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Clock, MapPin, User, CheckCircle } from 'lucide-react';
 import { formatDistanceToNow } from 'date-fns';
